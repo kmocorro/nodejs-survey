c8984f75d6df36c94c084a3c60ff57abb74e5c1c
vinx <kevinmocorro@gmail.com>
2017-10-29 06:13:22 +0800

    home done | post to db not yet

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,7 @@
 var bodyParser = require('body-parser');
 var mysqlLocal = require('../dbconfig/configLocal').poolLocal;
 var Promise = require('bluebird');
+var moment = require('moment');
 
 
 module.exports = function(app){
@@ -44,18 +45,55 @@ module.exports = function(app){
                         req.session.position = validate_user_obj[0].position;
                         req.session.department = validate_user_obj[0].department;
                         req.session.shift = validate_user_obj[0].shift;
+                        req.session.schedule = validate_user_obj[0].schedule;
 
                         res.send('ok');
 
                     } else {
-                        res.send(post_employee_id + ' was already participated. <br /><i>If you have not yet participated, <br />Please contact <a href="mailto:kevin.mocorro@sunpowercorp.com?Subject=Need%20Help" target="_blank">Kevin Mocorro </a>:)</i>');
+                        res.send(post_employee_id + ' was already participated. <br /><i>If you believed that you have not yet participated, <br />Please contact <a href="mailto:kevin.mocorro@sunpowercorp.com?Subject=Need%20Help,%20(Please%20use%20outlook%20when%20sending%20an%20email)" target="_blank">Kevin Mocorro </a>:)</i>');
                     }
                 } else {
-                    res.send('Sorry, ID number or Lastname is incorrect');
+                    res.send('Sorry, ID number or Lastname is incorrect <br/>(ex: 12345) <br/>(ex: Mocorro / Mocorro Jr / Mocorro III )');
                 }
             });
             connection.release(); // never forget;
         });
+    });
+
+
+    app.post('/survey/validate', function(req, res){
+        let post_radio = req.body.optradio;
+        let post_shuttleIn = req.body.shuttleIn;
+        let post_shuttleOut = req.body.shuttleOut;
+
+            console.log(post_radio);
+            console.log(post_shuttleIn);
+            console.log(post_shuttleOut);
+
+            if(post_radio == "No" && post){
+                mysqlLocal.getConnection(function(err, connection){
+                    connection.query({
+                        sql: 'UPDATE tbl_user_info SET willAttend=?, shuttleRoute=?, shuttleROuteOut=? isDone=? WHERE employee_id=?',
+                        values:[post_radio, 'NA', 'NA', 1, req.session.employee_id]
+                    }, function(err, results, fields){
+                        res.send('ok');
+                    });
+                    connection.release();
+                });
+            } else if(post_radio == "Yes") {
+                
+                mysqlLocal.getConnection(function(err, connection){
+                    connection.query({
+                        sql: 'UPDATE tbl_user_info SET willAttend=?, shuttleRoute=?, shuttleROuteOut=?, isDone=? WHERE employee_id=?',
+                        values:[post_radio, post_shuttleIn, post_shuttleOut, 1, req.session.employee_id]
+                    }, function(err, results, fields){
+                        res.send('ok');
+                    });
+                    connection.release();
+                });
+            
+            }
+
     });
 
     // to check how many employees pre-registered.
@@ -79,17 +117,44 @@ module.exports = function(app){
     
     // home page
     app.get('/home', checkAuth, function(req, res){
-
+        mysqlLocal.getConnection(function(err, connection){
+            connection.query({
+                sql: 'SELECT * FROM tbl_shuttle_info'
+            },  function(err, results, fields){
+                let shuttle_obj=[];
+                    for(let i=0;i<results.length;i++){
+                        shuttle_obj.push({
+                            shuttle: results[i].shuttleRoute
+                        });
+                    }
+                    // user names and details from session
+                    let user_session = [];
+                    if(req.session.firstname !== null && req.session.firstname !== null && req.session.lastname !== null && req.session.position !== null && req.session.department !== null && req.session.shift !== null){
+                        user_session.push({
+                            firstname: req.session.firstname,
+                            lastname: req.session.lastname,
+                            position: req.session.position,
+                            department: req.session.department,
+                            shift: req.session.shift,
+                            schedule: moment(req.session.schedule).format('MMMM Do YYYY, h A')
+                        });
+                    }
+                    
+                res.render('home',{user_session, shuttle_obj});
+            });
+            connection.release();
+        });
     });
 
     // thank you page
     app.get('/thankyou', checkAuth, function(req, res){
-
+        res.render('thankyou');
     });
 
     // logout
     app.get('/logout', function(req, res){
-
+        delete req.session.employee_id;
+        res.redirect('/');
     });
 
 
